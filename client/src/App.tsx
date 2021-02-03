7fe0630ee042c21fe24d75db71f479b26408ccdc
noahjb <nbalsmeyer@gmail.com>
2021-02-02 23:07:24 -0700

    Add initial localstorage auth hook up
    Add Auth0 package
    Add auth callback route handler
    Process and add auth info to local storage
    Clean up some less-performant logic by using useEffect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 import Home from './Home';
 import Profile from './Profile';
 import Nav from './Nav';
+import Auth from './Auth/Auth';
+import Callback from './Callback';
+import { History, Location, LocationState } from 'history';
 
-function App() {
+interface Props {
+  history: History<LocationState>;
+  location: Location;
+}
+
+function App(props: Props) {
   let [msg, setMsg] = useState('Loading');
+  let [auth, setAuth] = useState(new Auth(props.history));
   
-  fetch('/api')
-  .then(async res => {
-    setMsg(await res.text());
-    console.log(`Message from server: ${msg}`);
-  });
+  useEffect(() => {
+    fetch('/api')
+    .then(async res => {
+      setMsg(await res.text());
+      console.log(`Message from server: ${msg}`);
+    });
+  }, [msg]);
 
   return (
     <>
       <Nav />
       <div className='body'>
-        <Route path='/' exact component={ Home } />
+        <Route path='/' exact render={ props => <Home auth={auth} {...props} /> } />
+        <Route path='/callback' exact render={ props => <Callback auth={auth} {...props} /> } />
         <Route path='/profile' component={ Profile } />
       </div>
     </>
--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import Auth from './Auth/Auth';
+import { Link } from 'react-router-dom';
 
 interface Props {
-    
+    auth: Auth;
 }
 
 const Home = (props: Props) => {
+    const { isAuthenticated, login } = props.auth;
     return (
         <div>
             <h1>Home</h1>
+            { isAuthenticated() ?
+                <Link to='/profile'>View Profile</Link> :
+                <button onClick={login}>Log In</button>
+            }
         </div>
     )
 }
