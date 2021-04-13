a0ce1644552bb96dec0ea177530c59efa34a0594
noahjb <nbalsmeyer@gmail.com>
2021-04-13 17:10:56 -0600

    Modify code organization and improve/minimize scripts (#4)

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,12 @@
-import { Server } from './server/Server';
+import { Server } from './Server';
 import express, { Request, Response } from 'express';
 import path from 'path';
 
 const app = express();
-const port: number = parseInt(process.env.PORT as string) || 4000;
+app.set("port", process.env.PORT || 4000);
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '..', 'client/build')));
 
 const server = new Server(app);
-server.start(port);
+server.start(app.get("port"));
