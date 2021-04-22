2ed5f48f4a2c56c68dbfabe5f86d1e206804039e
noahjb <nbalsmeyer@gmail.com>
2021-04-22 16:03:44 -0600

    Add Jest for server unit tests and add to pipeline

---
+++ server/index.test.ts
@@ -0,0 +1,9 @@
+describe('dummy test', () => {
+    it('should pass when results match', () => {
+        expect(true).toBe(true);
+    });
+
+    it('should fail when results differ', () => {
+        expect(true).toBe(false);
+    });
+});
