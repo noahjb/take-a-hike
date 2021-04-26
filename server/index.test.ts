a9f56e8940bbb7f67e47427c906abdc7f2c50af4
noahjb <nbalsmeyer@gmail.com>
2021-04-26 16:15:46 -0600

    Remove dummy broken test and add parallel client unit test run

--- server/index.test.ts
+++ server/index.test.ts
@@ -2,8 +2,4 @@ describe('dummy test', () => {
     it('should pass when results match', () => {
         expect(true).toBe(true);
     });
-
-    it('should fail when results differ', () => {
-        expect(true).toBe(false);
-    });
 });
